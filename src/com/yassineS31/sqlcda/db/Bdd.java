9f4d49ef9fbcc3b07f4cbe43affaf0014844f228
yassine <y31m3x@gmail.com>
2025-02-26 10:08:31 +0100

    feat: bdd + connexion

---
+++ src/com/yassineS31/sqlcda/db/Bdd.java
@@ -0,0 +1,23 @@
+package com.yassineS31.sqlcda.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Bdd {
+    public class Bdd {
+        //Attribut paramètre BDD
+        static final String DB_URL = "jdbc:mysql://localhost:3307/javacda";
+        static final String USERNAME = "root";
+        static final String PASSWORD = "";
+        //Connexion à la BDD
+        private static Connection connexion;
+        static {
+            try {
+                connexion = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
